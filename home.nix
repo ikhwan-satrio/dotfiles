2b47800be5dab03ce409cd8de9f0e7c16b590d85
wanto-production <ikwansatria3974@gmail.com>
2025-12-15 20:08:06 +0700

    update

--- home.nix
+++ home.nix
@@ -16,8 +16,60 @@
 
   programs.zapzap.enable = true;
 
+  programs.vesktop = {
+    enable = true;
+    settings = {
+      appBadge = true;
+      arRPC = true;
+      checkUpdates = true;
+      customTitleBar = false;
+      disableMinSize = true;
+      minimizeToTray = true;
+      tray = true;
+      hardwareAcceleration = true;
+      discordBranch = "stable";
+      plugins = {
+        MessageLogger = {
+          enabled = true;
+          ignoreSelf = true;
+        };
+        FakeNitro.enabled = true;
+        AnonymiseFileNames.enable = true;
+        BetterSessions.enable = true;
+        BetterSettings.enable = true;
+        CallTimer.enable = true;
+        ClearURLs.enable = true;
+        CustomRPC.enable = true;
+        CustomIdle.enable = true;
+        DisableCallIdle = true;
+        FavoriteEmojiFirst.enable = true;
+        FixImagesQuality.enable = true;
+        FixYoutubeEmbeds.enable = true;
+        FriendsSince.enable = true;
+        GameActivityToggle.enable = true;
+        GifPaste.enable = true;
+        ImageZoom.enable = true;
+        KeepCurrentChannel.enable = true;
+        LastFMRichPresence.enable = true;
+        MessageLatency.enable = true;
+        ReadAllNotificationsButton.enable = true;
+        YoutubeAdblock.enable = true;
+        VolumeBooster.enable = true;
+        Unindent.enable = true;
+        NotTypingAnimation.enable = true;
+        SilentTyping.enable = true;
+      };
+    };
+  };
+
   gtk = {
     enable = true;
+
+    theme = {
+      name = "Sweet-Dark";
+      package = pkgs.sweet;
+    };
+
     iconTheme = {
       name = "Papirus-Dark";
       package = pkgs.papirus-icon-theme;
