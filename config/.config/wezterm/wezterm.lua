1ad9d6e8e27e7bc41f1c3d15de56b1ce3849c07e
wanto-production <ikwansatria3974@gmail.com>
2025-12-17 07:55:18 +0700

    wezterm change tabline themes

--- config/.config/wezterm/wezterm.lua
+++ config/.config/wezterm/wezterm.lua
@@ -14,10 +14,10 @@ config.window_padding = {
 }
 
 config.window_background_opacity = 1.0
-config.window_decorations = 'NONE'
 config.window_close_confirmation = 'NeverPrompt'
 
 -- Behavior
+config.enable_wayland = true -- jika pakai Wayland
 config.quit_when_all_windows_are_closed = true
 config.automatically_reload_config = true
 
@@ -35,7 +35,48 @@ config.scrollback_lines = 10000
 config.color_scheme = 'Noctalia'
 config.color_scheme_dirs = { wezterm.home_dir .. '/.config/wezterm/colors' }
 
-require('configs.plugins').apply_to_config(config)
+local tabline = wezterm.plugin.require 'https://github.com/michaelbrusegard/tabline.wez'
+tabline.setup {
+  options = {
+    icons_enabled = true,
+    theme = 'Catppuccin Mocha',
+    tabs_enabled = true,
+    theme_overrides = {},
+    section_separators = {
+      left = wezterm.nerdfonts.pl_left_hard_divider,
+      right = wezterm.nerdfonts.pl_right_hard_divider,
+    },
+    component_separators = {
+      left = wezterm.nerdfonts.pl_left_soft_divider,
+      right = wezterm.nerdfonts.pl_right_soft_divider,
+    },
+    tab_separators = {
+      left = wezterm.nerdfonts.pl_left_hard_divider,
+      right = wezterm.nerdfonts.pl_right_hard_divider,
+    },
+  },
+  sections = {
+    tabline_a = { 'mode' },
+    tabline_b = { 'workspace' },
+    tabline_c = { ' ' },
+    tab_active = {
+      'index',
+      { 'parent', padding = 0 },
+      '/',
+      { 'cwd', padding = { left = 0, right = 1 } },
+      { 'zoomed', padding = 0 },
+    },
+    tab_inactive = { 'index', { 'process', padding = { left = 0, right = 1 } } },
+    tabline_x = { 'ram', 'cpu' },
+    tabline_y = { 'datetime', 'battery' },
+    tabline_z = { 'domain' },
+  },
+  extensions = {},
+}
+tabline.apply_to_config(config)
+
+config.window_decorations = 'NONE' -- hanya border resize, tanpa title bar
+
 require('configs.keybinds').apply_to_config(config)
 
 return config
