6b038b5f6413cbc533b589698d7a369e4bf6998b
wanto-production <ikwansatria3974@gmail.com>
2025-12-22 12:22:38 +0700

    add grub themes

--- main-config/configuration.nix
+++ main-config/configuration.nix
@@ -45,6 +45,17 @@
         efiSupport = true;
         useOSProber = true;
         configurationLimit = 5;
+        theme = pkgs.stdenv.mkDerivation {
+          pname = "distro-grub-themes";
+          version = "3.1";
+          src = pkgs.fetchFromGitHub {
+            owner = "AdisonCavani";
+            repo = "distro-grub-themes";
+            rev = "v3.1";
+            hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
+          };
+          installPhase = "cp -r customize/nixos $out";
+        };
       };
       efi.canTouchEfiVariables = true;
     };
--- minimal-config/configuration.nix
+++ minimal-config/configuration.nix
@@ -39,9 +39,23 @@
       "rd.systemd.show_status=auto"
     ];
     loader = {
-      systemd-boot = {
+      grub = {
         enable = true;
-        configurationLimit = 5; # Hanya show 4 generasi terakhir
+        device = "nodev";
+        efiSupport = true;
+        useOSProber = true;
+        configurationLimit = 5;
+        theme = pkgs.stdenv.mkDerivation {
+          pname = "distro-grub-themes";
+          version = "3.1";
+          src = pkgs.fetchFromGitHub {
+            owner = "AdisonCavani";
+            repo = "distro-grub-themes";
+            rev = "v3.1";
+            hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
+          };
+          installPhase = "cp -r customize/nixos $out";
+        };
       };
       efi.canTouchEfiVariables = true;
     };
